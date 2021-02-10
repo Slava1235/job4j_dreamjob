2fb4dc11e735ff2a143e4e2f1027e83c97de913b
Slava1235 <starslava14@gmail.com>
2021-02-10 22:57:03 +0500

    2. CandidateSerlvet. Создание кандидата. [#442129]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -12,6 +12,7 @@ public class Store {
 
     private static final Store INST = new Store();
     private static final AtomicInteger POST_ID = new AtomicInteger(4);
+    private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
@@ -43,7 +44,7 @@ public class Store {
     }
 
     public void save(Candidate candidate) {
-        candidate.setId(POST_ID.incrementAndGet());
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
         candidates.put(candidate.getId(), candidate);
     }
 }
